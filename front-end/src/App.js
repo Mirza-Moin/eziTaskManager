d9480433d1289f4fccffb7b77412b4c8b5d00210
Mirza-Moin <moinmughal567@gmail.com>
2024-02-27 08:59:52 -0800

    Start Working Now

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App bg-red-500">
-    heloo how are you
+  Start Working Now
     </div>
   );
 }
